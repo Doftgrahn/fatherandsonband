4549826cad2beeaa416082cfe9e036fc82e88bd1
Simon grahn <simon.grahn@gmail.com>
2019-02-01 10:36:54 +0100

    added press and photos in gallery

--- src/js/app.js
+++ src/js/app.js
@@ -7,4 +7,16 @@ class App {
 
 window.addEventListener('load', () => {
   new App();
+   firebase.initializeApp(config);
+
 });
+
+// Firebase
+const config = {
+   apiKey: "AIzaSyDon9EBl4F6hxlzQJjqeuQQ_Fiby86bNGA",
+   authDomain: "father-and-son-band.firebaseapp.com",
+   databaseURL: "https://father-and-son-band.firebaseio.com",
+   projectId: "father-and-son-band",
+   storageBucket: "father-and-son-band.appspot.com",
+   messagingSenderId: "1079667065376"
+ };

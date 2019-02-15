67dd20d6ce2b231df58e35436fca91e2c119f875
Simon grahn <simon.grahn@gmail.com>
2019-02-15 12:44:37 +0100

    fix module and gallery

--- src/js/app.js
+++ src/js/app.js
@@ -1,36 +1,44 @@
+// import Glide from '@glidejs/glide';
+
+ import Gallery from './classes/gallery.js';
+
+
 class App {
   constructor() {
-    console.log('app.js');
-
-
-    const panels = document.querySelectorAll('.b-gallery__container-img');
-
-    panels.forEach(panel => panel.addEventListener('click', toggleOpen));
-    panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
-    // panels.forEach(panel => panel.addEventListener('mouseover', toggleOpen));
-
-
+   new Gallery();
 
   }
 }
+
+
+
 
 window.addEventListener('load', () => {
   new App();
+});
 
 
-})
 
 
 
-function toggleOpen() {
-  console.log('Hello');
-  this.classList.toggle('open');
-}
 
-function toggleActive(e) {
-  console.log(e.propertyName);
-  if (e.propertyName.includes('flex')) {
-    this.classList.toggle('open-active');
-    console.log('lalala');
-  }
-}
+
+//     const panels = document.querySelectorAll('.b-gallery__container-img');
+
+//
+// panels.forEach(panel => panel.addEventListener('click', toggleOpen));
+// panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
+//
+//
+// function toggleOpen() {
+//   console.log('Hello');
+//   this.classList.toggle('open');
+// }
+//
+// function toggleActive(e) {
+//   console.log(e.propertyName);
+//   if (e.propertyName.includes('flex')) {
+//     this.classList.toggle('open-active');
+//     console.log('lalala');
+//   }
+// }
---
+++ src/js/classes/gallery.js
@@ -0,0 +1,34 @@
+
+ class Gallery {
+   constructor() {
+
+     const mySwiper = new Swiper ('.swiper-container', {
+         // Optional parameters
+         // direction: 'vertical',
+         speed:400,
+         loop: true,
+
+         // If we need pagination
+         pagination: {
+           el: '.swiper-pagination',
+         },
+
+         // Navigation arrows
+         navigation: {
+           nextEl: '.swiper-button-next',
+           prevEl: '.swiper-button-prev',
+         },
+
+         // And if we need scrollbar
+         scrollbar: {
+           el: '.swiper-scrollbar',
+         },
+       })
+
+
+
+   }
+ }
+
+
+ export default Gallery;

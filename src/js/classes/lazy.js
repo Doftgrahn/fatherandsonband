03e5697f3f61ce319a47aed6c908fca1dadb5fd8
Simon grahn <simon.grahn@gmail.com>
2019-02-17 21:55:48 +0100

    test

--- src/js/classes/lazy.js
+++ src/js/classes/lazy.js
@@ -1,18 +1,11 @@
 
 
+
 class Lazy {
   constructor() {
 
 
-
-    window.addEventListener('load', function(){
-        const allimages= document.getElementsByTagName('img');
-        for (var i=0; i<allimages.length; i++) {
-            if (allimages[i].getAttribute('data-src')) {
-                allimages[i].setAttribute('src', allimages[i].getAttribute('data-src'));
-            }
-        }
-    }, false)
+  
 
 
   }

c18e3957fd00c845b439f7a0c02301363b92ecd3
Simon grahn <simon.grahn@gmail.com>
2019-02-22 16:05:36 +0100

    fix hamburger on mobile

--- src/js/classes/scroll.js
+++ src/js/classes/scroll.js
@@ -7,15 +7,21 @@ class Scroll {
       headerTargets.forEach(link => {
         link.addEventListener('click', (e) => {
           this.scroll(link, e);
+          this.removeHamburger();
         })
       });
     }
   }
-
   scroll(target, e) {
-     e.preventDefault();
+    e.preventDefault();
     let targetSection = target.getAttribute('data-scroll');
     document.getElementById(targetSection).scrollIntoView();
+  }
+  removeHamburger() {
+    this.hamburger = document.querySelector('.b-header__hamburgerContainer');
+    this.wrapper = document.querySelector('.b-header__container');
+    this.wrapper.classList.remove('b-header__container--active');
+    this.hamburger.classList.remove('active');
   }
 }
 
